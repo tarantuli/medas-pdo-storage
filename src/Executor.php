63579e7c55fd9f8572fb4549b2f5c81e7e330a8c
Patrick Morpey <patrick@morphp.nl>
2022-11-21 10:36:05 +0100

    Overall rewrite

---
+++ src/Executor.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Medas\PdoStorage;
+
+use Medas\PdoStorage\Exceptions\PdoDatabaseException;
+use Medas\PdoStorage\Queries\Query;
+
+class Executor
+{
+    public function execute(\PDO $pdo, Query $query): void
+    {
+        $this->serializeArguments($query);
+
+        try {
+            $statement = $pdo->prepare($query->query);
+            $statement->execute($query->serializedArguments);
+
+            $query->setStatement(new Statement($statement));
+        }
+        catch (\Exception|\Error $e) {
+            throw new PdoDatabaseException($e->getMessage(), $query);
+        }
+
+        if ($onComplete = $query->onComplete()) {
+            $onComplete($query->storage());
+        }
+    }
+
+    private function serializeArguments(Query $query): void
+    {
+        foreach ($query->arguments as $argument) {
+            if ($argument instanceof \DateTime) {
+                $argument = $argument->format('Y-m-d H:i:s');
+            }
+
+            if (is_bool($argument)) {
+                $argument = (int) $argument;
+            }
+
+            $query->serializedArguments[] = $argument;
+        }
+    }
+}
