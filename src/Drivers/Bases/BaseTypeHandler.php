60dec8acff6d9fdf8fee4d69631a4bdcc794f0f2
Patrick Morpey <patrick@morphp.nl>
2023-02-27 16:23:01 +0100

    Missing minus signs

--- src/Drivers/Bases/BaseTypeHandler.php
+++ src/Drivers/Bases/BaseTypeHandler.php
@@ -69,10 +69,10 @@ abstract class BaseTypeHandler implements TypeHandler
             $field->minValue >= 0 && $field->maxValue <= Integer::UNSIGNED_4_BYTE_MAX => 'int unsigned',
             $field->minValue >= 0 && $field->maxValue <= Integer::UNSIGNED_8_BYTE_MAX => 'bigint unsigned',
 
-            $field->minValue >= Integer::SIGNED_1_BYTE_MAX && $field->maxValue <= Integer::SIGNED_1_BYTE_MAX => 'tinyint',
-            $field->minValue >= Integer::SIGNED_2_BYTE_MAX && $field->maxValue <= Integer::SIGNED_2_BYTE_MAX => 'mediumint',
-            $field->minValue >= Integer::SIGNED_3_BYTE_MAX && $field->maxValue <= Integer::SIGNED_3_BYTE_MAX => 'int',
-            $field->minValue >= Integer::SIGNED_4_BYTE_MAX && $field->maxValue <= Integer::SIGNED_4_BYTE_MAX => 'bigint',
+            $field->minValue >= -Integer::SIGNED_1_BYTE_MAX && $field->maxValue <= Integer::SIGNED_1_BYTE_MAX => 'tinyint',
+            $field->minValue >= -Integer::SIGNED_2_BYTE_MAX && $field->maxValue <= Integer::SIGNED_2_BYTE_MAX => 'mediumint',
+            $field->minValue >= -Integer::SIGNED_3_BYTE_MAX && $field->maxValue <= Integer::SIGNED_3_BYTE_MAX => 'int',
+            $field->minValue >= -Integer::SIGNED_4_BYTE_MAX && $field->maxValue <= Integer::SIGNED_4_BYTE_MAX => 'bigint',
 
             default => throw new \Exception('out of bounds value range'),
         };
