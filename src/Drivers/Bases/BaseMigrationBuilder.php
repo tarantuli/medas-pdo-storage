bdb12acd66ad8fb22ec1c5f8c8027d1758e29ff7
Patrick Morpey <patrick@morphp.nl>
2023-08-07 16:07:57 +0200

    Updated packages, cleaned up code

--- src/Drivers/Bases/BaseMigrationBuilder.php
+++ src/Drivers/Bases/BaseMigrationBuilder.php
@@ -6,11 +6,8 @@ namespace Medas\PdoStorage\Drivers\Bases;
 
 use Medas\FileBuilder\PhpClass\MethodDefinition;
 use Medas\PdoStorage\Database;
-use Medas\PdoStorage\Exceptions\StorageIsNotDatabase;
 use Medas\PdoStorage\Queries\{Query, QueryCollection};
-use Medas\StorageManager\Interfaces\Storage;
 use Medas\StorageManager\Migrations\MigrationBuilder;
-use Medas\StorageManager\StorageManager;
 use Medas\StorageManager\Structure\Blueprint;
 use Medas\StorageManager\Structure\Changes\ChangeFinder;
 use Medas\StorageManager\Structure\EntityStructureFinder;
@@ -18,30 +15,20 @@ use Medas\StorageManager\UnitOfWork\Priority;
 
 abstract class BaseMigrationBuilder implements MigrationBuilder
 {
-    private Database $database;
-
     public function __construct(
         private readonly ChangeFinder          $changeFinder,
         private readonly EntityStructureFinder $entityStructureFinder,
-        private readonly StorageManager        $storageManager,
+        private readonly Database              $database,
     )
     {
     }
 
     public function build(
-        Storage          $storage,
         string           $className,
         MethodDefinition $migrateMethod,
         MethodDefinition $undoMethod,
     ): bool
     {
-        /** @noinspection PhpConditionAlreadyCheckedInspection */
-        if (!$storage instanceof Database) {
-            throw new StorageIsNotDatabase($this->storageManager->getName($storage));
-        }
-
-        $this->database = $storage;
-
         $expectedStructure = $this->entityStructureFinder->find($className);
         $queries = $this->buildQueries($expectedStructure);
 
@@ -75,7 +62,7 @@ PHP;
         $existingStructure = $driver->tableStructureFinder()->find($this->database->store($expectedStructure->name()));
 
         if ($existingStructure === null) {
-            return $this->database->controller()->actionBuilder()->createStore($expectedStructure);
+            return $driver->createTableBuilder()->create($expectedStructure);
         }
         else {
             $changes = $this->changeFinder->find($expectedStructure, $existingStructure);
