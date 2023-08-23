44c574eb573059319110282b64c0146fac06740d
Patrick Morpey <patrick@morphp.nl>
2023-08-23 09:24:26 +0200

    WIP

--- src/PdoStorageController.php
+++ src/PdoStorageController.php
@@ -54,22 +54,26 @@ class PdoStorageController implements StorageController
 
     public function store(string $name, Storage $storage = null): Table
     {
-        return $this->getDatabaseController($storage ?? $this->defaultDatabase)->driverHandler->table($storage, $name);
+        return $this->getDatabaseController($storage ?? $this->defaultDatabase)->driverHandler
+            ->table($storage, $name);
     }
 
     public function actionBuilders(Storage $storage = null): ActionBuilders
     {
-        return $this->getDatabaseController($storage ?? $this->defaultDatabase)->driverHandler->queryBuilders();
+        return $this->getDatabaseController($storage ?? $this->defaultDatabase)->driverHandler
+            ->queryBuilders();
     }
 
     public function serializer(Storage $storage = null): Serializer
     {
-        return $this->getDatabaseController($storage ?? $this->defaultDatabase)->driverHandler->serializer();
+        return $this->getDatabaseController($storage ?? $this->defaultDatabase)->driverHandler
+            ->serializer();
     }
 
     public function migrationBuilder(Storage $storage = null): MigrationBuilder
     {
-        return $this->getDatabaseController($storage ?? $this->defaultDatabase)->driverHandler->migrationBuilder();
+        return $this->getDatabaseController($storage ?? $this->defaultDatabase)->driverHandler
+            ->migrationBuilder();
     }
 
     public function lastGeneratedValue(Storage $storage = null): int|null
@@ -81,7 +85,9 @@ class PdoStorageController implements StorageController
 
     public function hasStore(Store $store, Storage $storage = null): bool
     {
-        $query = $this->getDatabaseController($storage ?? $this->defaultDatabase)->driverHandler->queryBuilders()->showTables()->build($storage, $storage->name());
+        $query = $this->getDatabaseController($storage ?? $this->defaultDatabase)->driverHandler
+            ->queryBuilders()->showTables()->build($storage, $storage->name());
+
         $query->execute();
 
         return $query->recordSet()->hasRecords();
@@ -89,7 +95,8 @@ class PdoStorageController implements StorageController
 
     public function recordFetchers(Storage $storage = null): RecordFetchers
     {
-        return $this->getDatabaseController($storage ?? $this->defaultDatabase)->driverHandler->recordFetchers();
+        return $this->getDatabaseController($storage ?? $this->defaultDatabase)->driverHandler
+            ->recordFetchers();
     }
 
     public function quote(Database $database, string $identifier): string
@@ -101,57 +108,4 @@ class PdoStorageController implements StorageController
     {
         return $this->getDatabaseController($database)->driverHandler->escape($database, $value);
     }
-
-    /*        private readonly QueryExecutor                 $executor,
-            private readonly JoinTableManager              $joinTableManager,*/
-    //    public function execute(Query $query): void
-    //    {
-    //        $this->executor->execute($this->getDatabaseController($query->storage())->pdo, $query);
-    //    }
-    //
-    /*    public function escapeValue(mixed $value): string
-        {
-            if (null === $value) {
-                return 'null';
-            }
-
-            if (is_object($value) && enum_exists($value::class)) {
-                $value = $value->value;
-            }
-
-            if (is_bool($value)) {
-                $value = (int) $value;
-            }
-
-            return $this->pdo->quote((string) $value);
-        }
-    */
-    /*    public function fetchAll(Store $store, array $filters, Storage $storage = null): array|null
-        {
-            $storage ??= $this->defaultDatabase;
-    
-            $query = $this->getDatabaseController($storage ?? $this->defaultDatabase)->driverHandler->queryBuilders()->select([$store], $filters);
-            $query->execute();
-    
-            return $query->recordSet()->fetchRecords();
-        }
-    
-        public function fetchCollectionRecord(Store $store, object $entity, Property $property, Storage $storage = null): iterable
-        {
-            $storage ??= $this->defaultDatabase;
-    
-            $joinTable = $this->joinTableManager->determineName($storage->name(), $property->name);
-    
-            return $this->fetchAll($this->store($joinTable, $storage ?? $this->defaultDatabase), ['id' => $entity]);
-        }
-    
-        public function fetchRecord(Store $store, array $filters, Storage $storage = null): Record|null
-        {
-            $storage ??= $this->defaultDatabase;
-    
-            $query = $this->getDatabaseController($storage ?? $this->defaultDatabase)->driverHandler->queryBuilders()->select([$store], $filters);
-            $query->execute();
-    
-            return $query->recordSet()->fetchRecord();
-        }*/
 }
