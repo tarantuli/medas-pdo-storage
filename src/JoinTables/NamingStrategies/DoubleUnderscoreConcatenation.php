71838ab392be5ae553d781285982efa4713b52bf
Patrick Morpey <patrick@morphp.nl>
2023-09-13 09:39:56 +0200

    Made class service

--- src/JoinTables/NamingStrategies/DoubleUnderscoreConcatenation.php
+++ src/JoinTables/NamingStrategies/DoubleUnderscoreConcatenation.php
@@ -4,9 +4,11 @@ declare(strict_types=1);
 
 namespace Medas\PdoStorage\JoinTables\NamingStrategies;
 
+use Medas\Core\Attributes\Service;
 use Medas\PdoStorage\JoinTables\NamingStrategy;
 
-class DoubleUnderscoreConcatenation implements NamingStrategy
+#[Service]
+readonly class DoubleUnderscoreConcatenation implements NamingStrategy
 {
     public function determine(string $sourceTable, string $property): string
     {
