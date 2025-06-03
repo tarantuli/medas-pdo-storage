1436f7e7de727cb5ec2f804ac570215175bc0649
Patrick Morpey <patrick@morphp.nl>
2025-06-03 14:49:34 +0200

    Implemented hasStore()

--- src/PdoStorageController.php
+++ src/PdoStorageController.php
@@ -131,7 +131,9 @@ class PdoStorageController implements StorageController
 
     public function hasStore(Store $store, Storage $storage = null): bool
     {
-        return $this->getStores() !== [];
+        return (bool) array_filter($this->getStores($storage), function ($aStore) use ($store) {
+            return $store->name() === $aStore->name();
+        });
     }
 
     public function recordFetchers(): RecordFetchers
