47190cbc6108e6f328516e87b16a1368c32b02ae
Patrick Morpey <patrick@morphp.nl>
2026-06-15 13:29:02 +0200

    Refactored `DriverHandlerManager` to leverage constructor-based dependency injection, optimize handler discovery, and remove unused `addHandler` method.

--- src/Drivers/DriverHandlerManager.php
+++ src/Drivers/DriverHandlerManager.php
@@ -4,28 +4,41 @@ declare(strict_types=1);
 
 namespace Medas\PdoStorage\Drivers;
 
-use Medas\Core\Attributes\Service;
+use Medas\Core\{Attributes\Service, Interfaces\CacheManager, Interfaces\ImplementorFinder};
 use Medas\PdoStorage\Exceptions\DriverNotImplemented;
 
 #[Service]
-class DriverHandlerManager
+readonly class DriverHandlerManager
 {
     /** @var DriverHandler[] */
-    private array $driverHandlers = [];
+    private array $driverHandlers;
 
-    private bool $isSorted = false;
+    public function __construct(
+        private CacheManager      $cacheManager,
+        private ImplementorFinder $implementorFinder,
+    )
+    {
+        $this->driverHandlers = namesToServices($this->cacheManager->get()->get(
+            __CLASS__,
+            fn() => $this->discoverHandlers()
+        ));
+    }
+
+    /** @return class-string[] */
+    private function discoverHandlers(): array
+    {
+        $driverHandlers = namesToServices($this->implementorFinder->find(DriverHandler::class));
+
+        usort(
+            $driverHandlers,
+            fn(DriverHandler $a, DriverHandler $b) => -($a->priority() <=> $b->priority())
+        );
+
+        return servicesToNames($driverHandlers);
+    }
 
     public function find(string $driverName): DriverHandler
     {
-        if (!$this->isSorted) {
-            usort(
-                $this->driverHandlers,
-                fn(DriverHandler $a, DriverHandler $b) => -($a->priority() <=> $b->priority())
-            );
-
-            $this->isSorted = true;
-        }
-
         foreach ($this->driverHandlers as $driverHandler) {
             if ($driverHandler->canHandle($driverName)) {
                 return $driverHandler;
@@ -33,11 +46,5 @@ class DriverHandlerManager
         }
 
         throw new DriverNotImplemented($driverName);
-    }
-
-    public function addHandler(DriverHandler $handler): void
-    {
-        $this->driverHandlers[] = $handler;
-        $this->isSorted = false;
     }
 }
